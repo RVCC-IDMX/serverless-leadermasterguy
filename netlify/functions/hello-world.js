8cc5baf53e3cb131ae8cf0f915dda58567070877
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-11-27 20:29:54 -0500

    Implement Chalk/Luxon

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,8 +1,11 @@
-exports.handler = async function () {
+const chalk = require('chalk');
+const { DateTime } = require('luxon');
+
+exports.handler = async () => {
+  const date = DateTime.now();
+  console.log(chalk.blue(`${date}: Hello from Netlify Functions!`));
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello world!',
-    }),
+    body: JSON.stringify({ message: 'Hello World!' }),
   };
 };
--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,12 +1,23 @@
 // mod.cjs
 // eslint-disable-next-line no-shadow
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const chalk = require('chalk');
+const { DateTime } = require('luxon');
 
-exports.handler = async function (event, context) {
+exports.handler = async (event) => {
   const eventBody = JSON.parse(event.body);
+  const date = DateTime.now();
+  const color = eventBody.region === 'kanto' ? chalk.red : chalk.green;
+
+  console.log(color(`${date}: Fetching data from PokeAPI`));
+  console.log(color(`\teventBody.region: ${eventBody.region}`));
+
   const POKE_API = `https://pokeapi.co/api/v2/pokedex/${eventBody.region}`;
+
   const response = await fetch(POKE_API);
   const data = await response.json();
+  console.log(color(`\tNumber of entries: ${data.pokemon_entries.length}`));
+
   return {
     statusCode: 200,
     body: JSON.stringify({
